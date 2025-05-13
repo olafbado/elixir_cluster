3e4bd0dedf580142e71a8e87ac760ae93d809030
olafbado <niebiel1@gmail.com>
2025-05-13 10:53:43 +0100

    Update libcluster configuration for prod to use DNS polling strategy

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,15 @@ import Config
 
 config :libcluster,
   topologies: [
-    local_gossip: [
-      strategy: Cluster.Strategy.Gossip
+    dns_poll: [
+      strategy: Cluster.Strategy.DNSPoll,
+      config: [
+        polling_interval: 5_000,
+        # <service_name>.<namespace>
+        query: "elixir_cluster.elixir_cluster",
+        # set by the RELEASE_NODE env var
+        node_basename: "elixir_cluster",
+        record_type: :a
+      ]
     ]
   ]
